3f8ed487338d37ab1719d7eb2c5d1002597f4672
avinashyeccalluri <yeccaluriavinash@gmail.com>
2020-09-02 02:56:39 +0530

    updates

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-let navWrapper = document.querySelector('.nav-wrapper'),
-navToogler =  document.querySelector('.nav-toogler')
+// let navWrapper = document.querySelector('.nav-wrapper'),
+// navToogler =  document.querySelector('.nav-toogler')
 
-navToogler.addEventListener('click', function (event) {
-navWrapper.classList.toggle('active')
-})
+// navToogler.addEventListener('click', function (event) {
+// navWrapper.classList.toggle('active')
+// })
+
+
+
+
+
+// // Wrap every letter in a span
+// var textWrapper = document.querySelector('.ml16');
+// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+AOS.init({
+    duration: 1200,
+  })
